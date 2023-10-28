7ab84b369a47d6139d8bb5e3d77d9a9e5c7eb446
Chandru <chandrusakthivelc@gmail.com>
2023-10-28 19:27:11 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];// now the cart variable can be use in any other files without naming conflicts, through by export module.
+export const cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2,
+
+}, {
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1
+}];// now the cart variable can be use in any other files without naming conflicts, through by export module.
 
 
 // the below function is for adding products into the cart.
