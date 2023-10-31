d1da2edcdc750e5f1f74b8887984f650ff9ae7bd
Chandru <chandrusakthivelc@gmail.com>
2023-10-31 19:03:21 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,31 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));// getItem is used for get the item from the localstorage when we saved a value/item in the localstorage through the setItem. 
+
+// parse is method and it's used for to convert string into orginal form/ html form. because localstorage only store/save string value.
+
+// if the user use the site first time, there will no any item/data in the localstorage. so defaulty we save data/item in the cart through the below code. 
+
+
+if(!cart){// it is the truthy statement that if the cart is null then default value/item should be display in page and the data/item are store in cart.
+ cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 
 }, {
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
-}];// now the cart variable can be use in any other files without naming conflicts, through by export module.
+}];
+}
+
+// now the cart variable can be use in any other files without naming conflicts, through by export module.
+
+// saving a cart in the localstorage
+
+/* 
+whenever we update the cart, we need to save it to localStorage, so it doesn't get refresh when we reload the page.
+*/
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));//localstorage can only save strings. so we convert the cart into string. 
+}
 
 
 // the below function is for adding products into the cart.
@@ -34,6 +54,8 @@ export function addtoCart(productId){
         quantity:quantitySelector
       });
     }
+
+    saveToStorage();
 }
 
 
@@ -54,4 +76,6 @@ export function removeFromCart(productId){
   });
   // replace the old cart with the new Cart.
   cart=newCart;
+
+  saveToStorage();
 }
