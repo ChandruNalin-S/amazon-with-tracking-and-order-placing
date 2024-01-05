f68fa8bfd2427cd7879c811ea9973f3ea17ee7e9
Chandru <chandrusakthivelc@gmail.com>
2024-01-05 19:31:29 +0530

    save data and generate html for delivery option

--- data/cart.js
+++ data/cart.js
@@ -9,10 +9,11 @@ if(!cart){// it is the truthy statement that if the cart is null then default va
  cart = [{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
-
+  deliveryOptionId:'1'
 }, {
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1
+  quantity:1,
+  deliveryOptionId:'2'
 }];
 }
 
@@ -51,7 +52,8 @@ export function addtoCart(productId){
       cart.push({
         productId:productId,
         quantity:1,
-        quantity:quantitySelector
+        quantity:quantitySelector,
+        deliverOptionId:'1'
       });
     }
 
